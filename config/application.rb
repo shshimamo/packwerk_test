15a63f80fe63ddd3ed5021cbc86ae133136656ca
hiroya.shimamoto <shshimamo@gmail.com>
2021-09-20 03:26:59 +0900

    mkdir packages directory

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module PackwerkTest
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.paths.add 'packages', glob: '*/{app/*,app/*/concerns,lib}', eager_load: true
   end
 end
